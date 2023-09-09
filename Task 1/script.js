d14555374f079b7c1cd77649c4e327a2e03d558b
Eko Gift <ekogift2000@gmail.com>
2023-09-09 14:38:53 +0100

    make some changes

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,12 +1,8 @@
 'use strict';
 
-const slackName = document.querySelector('.slack-profile-name');
 const currentDay = document.querySelector('.current-date');
 const currentUTCTime = document.querySelector('.current-time');
-const hngTrack = document.querySelector('.hng-track');
 const curDayIndex = new Date().getUTCDay();
-
-const githubLink = document.querySelector('.github-url');
 
 const week = [
   'Sunday',
@@ -18,15 +14,6 @@ const week = [
   'Saturday',
 ];
 
-slackName.setAttribute('data-testid', slackName.textContent);
-
 currentDay.textContent = week[curDayIndex];
-currentDay.setAttribute('data-testid', currentDay.textContent);
 
 currentUTCTime.textContent = Date.now();
-currentUTCTime.setAttribute('data-testid', currentUTCTime.textContent);
-console.log(currentUTCTime);
-
-hngTrack.setAttribute('data-testid', hngTrack.textContent);
-
-githubLink.setAttribute('data-testid', githubLink.href);
